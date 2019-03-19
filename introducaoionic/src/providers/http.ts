29668968264910b70b4e508f8f09995ea7bda941
Richard Kanigoski <richardkanigoski1@gmail.com>
2019-03-18 21:11:28 -0300

    Criado provider Htttp

--- introducaoionic/src/providers/http.ts
+++ introducaoionic/src/providers/http.ts
@@ -1,34 +1,49 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
+import { Http, Headers, Response} from '@angular/http';
 import 'rxjs/add/operator/timeout';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 
 @Injectable()
-export class httpProvider {
-  public url : string;
-  public token : string;
+export class HttpProvider {
+    public url : string;
+    public token : string;
 
-  // private const TIMEOUT : number = 15000;
+    private TIMEOUT = 15000;
 
-  constructor() {
-    this.url = null;
-    this.token = null;
-  }
+    constructor(){
+        this.url = null;
+        this.token = null;
+    }
 
-  public get() {
+    private prepareHeaders(contentType : boolean){
+        var headers = new Headers();
+        headers.append('Accept', 'application/json');
 
-  }
+        if (contentType){
+          headers.append('Content-Type', 'application/json');
+        }
 
-  public post(obj : any) {
+        if (this.token == ''){
+          this.token = null;
+        }
 
-  }
+        if (this.token != null){
+          headers.append('Authorization', 'bearer ' + this.token);
+        }
 
-  public put(obj : any) {
+        return headers;
+      }
+    public get(){
 
-  }
+    }
+    public post(obj : any){
 
-  public delete(obj : any) {
+    }
+    public put(obj : any){
 
-  }
+    }
+    public delete(obj : any){
+
+    }
 }
