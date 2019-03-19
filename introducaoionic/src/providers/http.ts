bdd1baa1ce37800da93d96d475f679d78f42d6e3
Richard Kanigoski <richardkanigoski1@gmail.com>
2019-03-18 21:02:42 -0300

    Criação do sistema de login com requisição no servidor

---
+++ introducaoionic/src/providers/http.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import 'rxjs/add/operator/timeout';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+
+@Injectable()
+export class httpProvider {
+  public url : string;
+  public token : string;
+
+  // private const TIMEOUT : number = 15000;
+
+  constructor() {
+    this.url = null;
+    this.token = null;
+  }
+
+  public get() {
+
+  }
+
+  public post(obj : any) {
+
+  }
+
+  public put(obj : any) {
+
+  }
+
+  public delete(obj : any) {
+
+  }
+}
---
+++ introducaoionic/src/providers/login.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Login{
+
+  constructor(){
+
+  }
+
+  public login(userName : String, password: String) {
+
+  }
+}
